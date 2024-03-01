872f8576b7b9ffe1ca2d80bb2f99b35fe946e211
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-03-02 01:10:39 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -21,22 +21,7 @@ const Card = () => {
   // });
   return (
     <div className="box">
-      // {datas.map((mydata) => {
-      //   return (
-      //     <div className="card" style={{width: '18rem'}}>
-      //       <img src={mydata.img} className="card-img-top" alt="img not found" />
-      //       <div className="card-body">
-      //         <h5 className="card-title">{mydata.title}</h5>
-      //         <p className="card-text">
-      //         {mydata.desc}
-      //         </p>
-      //         <a href={mydata.downloadurl} className="btn btn-primary" target="_blank">
-      //           download
-      //         </a>
-      //       </div>
-      //     </div>
-      //   );
-      // })}
+    
        heeelo
     </div>
   );
