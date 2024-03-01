4ac7625bc8ca798fa4bf77a828f57a2931df83cd
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-03-02 01:08:12 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,44 +1,44 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// const Card = () => {
-//   const initalvalue = [{ title: "Aniket King", img: "24243535", desc: "fsg",
-//   downloadurl: "dsf"}];
-//   const [datas, setdatas] = useState(initalvalue);
-//   const Fetchdata = async () => {
-//     try {
-//       //    const url = '/api/data'
-//       const data = await fetch("https://card-data-seven.vercel.app/api/data");
-//       const parsedata = await data.json();
-//       setdatas(parsedata);
-//     } catch (e) {
-//       console.log("failed to fetch data");
-//       console.log('fuck you")
-//       console.log(e);
-//     }
-//   };
-//   useEffect(() => {
-//     Fetchdata();
-//   });
-//   return (
-//     <div className="box">
-//       {datas.map((mydata) => {
-//         return (
-//           <div className="card" style={{width: '18rem'}}>
-//             <img src={mydata.img} className="card-img-top" alt="img not found" />
-//             <div className="card-body">
-//               <h5 className="card-title">{mydata.title}</h5>
-//               <p className="card-text">
-//               {mydata.desc}
-//               </p>
-//               <a href={mydata.downloadurl} className="btn btn-primary" target="_blank">
-//                 download
-//               </a>
-//             </div>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
+const Card = () => {
+  // const initalvalue = [{ title: "Aniket King", img: "24243535", desc: "fsg",
+  // downloadurl: "dsf"}];
+  // const [datas, setdatas] = useState(initalvalue);
+  // const Fetchdata = async () => {
+  //   try {
+  //     //    const url = '/api/data'
+  //     const data = await fetch("https://card-data-seven.vercel.app/api/data");
+  //     const parsedata = await data.json();
+  //     setdatas(parsedata);
+  //   } catch (e) {
+  //     console.log("failed to fetch data");
+  //     console.log('fuck you")
+  //     console.log(e);
+  //   }
+  // };
+  // useEffect(() => {
+  //   Fetchdata();
+  // });
+  return (
+    <div className="box">
+      // {datas.map((mydata) => {
+      //   return (
+      //     <div className="card" style={{width: '18rem'}}>
+      //       <img src={mydata.img} className="card-img-top" alt="img not found" />
+      //       <div className="card-body">
+      //         <h5 className="card-title">{mydata.title}</h5>
+      //         <p className="card-text">
+      //         {mydata.desc}
+      //         </p>
+      //         <a href={mydata.downloadurl} className="btn btn-primary" target="_blank">
+      //           download
+      //         </a>
+      //       </div>
+      //     </div>
+      //   );
+      // })}
+    </div>
+  );
+};
 
-// export default Card;
+export default Card;
