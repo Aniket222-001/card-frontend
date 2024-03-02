0b667a7a603f39d174df797416d4b70143f66710
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-03-02 20:08:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
 import {useState} from 'react'
 import axios from 'axios'
-// import Login from './Login'
+import Login from './Login'
 import { Routes,Route } from 'react-router-dom';
 
-
 function App() {
+  
   const [title,settitle] = useState();
   const [img,setimg] = useState(' ')
   const [desc,setdesc] = useState(' ')
   const [downloadurl,setdownloadurl] = useState(' ')
-  axios.defaults.withCredentials = true;
 
   const changehandler = async(e)=>{
     e.preventDefault()
-    await axios.post('https://card-data-3go1.vercel.app/login',{title,img,desc,downloadurl})
-    .then((result)=>{
-      console.log('data saved successfully')
+    const result = await axios.post('https://card-data-3go1.vercel.app/login',{title,img,desc,downloadurl})
+    .then(()=>{                                                                                   
+      console.log('data saved  successfully')
       console.log(result)
     })
     .catch((e)=>{
@@ -28,20 +27,23 @@ function App() {
     <div className="App">
       <h1>Hello</h1>
       <form action="" onSubmit={changehandler} className=''>
-       <label htmlFor="">tfsdfsdditle</label>
+       <label htmlFor="">title</label>
        <input type="text" name="" id="" onChange={(e)=>settitle(e.target.value)} />
        <label htmlFor="">img</label>
        <input type="text" name="" id=""  onChange={(e)=>setimg(e.target.value)}/>
+       <div>
        <label htmlFor="" className='desc'>Description</label>
        <input type="text" name="" id=""  onChange={(e)=>setdesc(e.target.value)}/>
+
+       </div>
        <label htmlFor="">Download url</label>
        <input type="text"  onChange={(e)=>setdownloadurl(e.target.value)}/>
        <button type='submit'>submit</button>
        </form>
-      // <Routes>
-      //   <Route path ='/home' element={<Login/>}>Home</Route>
-      // </Routes>
-        // <Login/>
+      <Routes>
+        <Route path ='/Login' element={<Login/>}>Home</Route>
+      </Routes>
+        <Login/>
     </div>
   );
 }
